390600f553f0b190fe8cb1a4be5dbefcae81ca60
Iulian Serbanoiu <undergraver@gmail.com>
2019-01-31 21:30:36 +0200

    model creation

--- tcproject/tcapp/models.py
+++ tcproject/tcapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class TCUser(models.Model):
+    email = models.EmailField()
+    fullName = models.TextField()
+
+class TCPost(models.Model):
+    userId = models.ForeignKey(TCUser,on_delete=models.CASCADE)
+    pubDate = models.DateField(auto_now_add=True)
+
+class TCLike(models.Model):
+    postId = models.ForeignKey(TCPost,on_delete=models.CASCADE)
+    userId = models.ForeignKey(TCUser,on_delete=models.CASCADE)
