64d13ea1058e3d37e8c6a21c92d14319428c9c0a
Iulian Serbanoiu <undergraver@gmail.com>
2019-01-31 22:00:50 +0200

    final model. migraton is created with the following command:

--- tcproject/tcapp/models.py
+++ tcproject/tcapp/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class TCUser(models.Model):
+class User(models.Model):
     email = models.EmailField()
     fullName = models.TextField()
 
-class TCPost(models.Model):
-    userId = models.ForeignKey(TCUser,on_delete=models.CASCADE)
+class Post(models.Model):
+    userId = models.ForeignKey(User,on_delete=models.CASCADE)
     pubDate = models.DateField(auto_now_add=True)
 
-class TCLike(models.Model):
-    postId = models.ForeignKey(TCPost,on_delete=models.CASCADE)
-    userId = models.ForeignKey(TCUser,on_delete=models.CASCADE)
+class Like(models.Model):
+    postId = models.ForeignKey(Post,on_delete=models.CASCADE)
+    userId = models.ForeignKey(User,on_delete=models.CASCADE)
