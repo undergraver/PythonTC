56c1e5aeec3268bbe643696d8e04975a3bb9e763
Iulian Serbanoiu <undergraver@gmail.com>
2019-02-03 18:28:07 +0200

    updated model to integrate hunter and clearbit (not functional)

--- tcproject/tcapp/models.py
+++ tcproject/tcapp/models.py
@@ -1,10 +1,54 @@
 from django.db import models
 
+import pyhunter
+import clearbit
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+import os
+
+def CheckIfEmailIsGood(value):
+    information = None
+    try:
+        ph = pyhunter.PyHunter(os.getenv('HUNTER_API_KEY','bogus'))
+        information = ph.email_verifier(value)
+    except:
+        # ignore in case some connectivity issue happens
+        pass
+
+    if information is not None:
+        if information['result'] == 'undeliverable':
+            raise ValidationError(
+            _('Hunter says %s is an invalid email.') % (value)
+            )
+
 class User(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True,validators=[CheckIfEmailIsGood])
     password = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200,blank=True)
     last_name = models.CharField(max_length=200,blank=True)
+
+    def clean(self):
+        super(User,self).clean()
+        # complete if first_name and last_name is empty
+        if self.first_name == "" and self.last_name == "":
+            clearbit.key = os.getenv('CLEARBIT_KEY','bogus')
+            try:
+                response = clearbit.Enrichment.find(email=self.email, stream=True)
+                print(response)
+                if 'person' in response:
+                    self.first_name = response['person']['name']['givenName']
+                    self.last_name = response['person']['name']['familyName']
+            except:
+                pass
+                # test
+                #self.first_name = "no clearbit api key - signup failed"
+                #self.last_name = "no nice last name, sorry"
+ 
+    def save(self, **kwargs):
+        self.clean()
+        return super(User, self).save(**kwargs)
 
     def __str__(self):
         return self.email
